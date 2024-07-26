12846f9efabe373f11c03be067cf858aca470b3a
omozuas <iyanuomozua@gmail.com>
2024-07-26 16:02:41 +0100

    first update

---
+++ android/app/src/main/kotlin/com/example/touchsync/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.touchsync
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        child: Text('Welcome Team I'),
+      ),
+    ));
+  }
+}
