8379308af896a7277b15b75b6610d6000175c3f0
fidelmak <holubakin@gmail.com>
2024-07-30 20:57:08 +0100

    updated the success screen

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: const ReadTagPage(), //BottomNavigation(),
+        home: const BottomNavigation(),
       ),
     );
   }
